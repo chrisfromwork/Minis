8cc377e3e09ad6355cdf8ba9cb81adfcc0269403
chrisfromwork <c.m.barth@outlook.com>
2021-12-14 16:50:11 -0800

    move device lookup to separate thread

--- Packages/jp.keijiro.minis/Runtime/Internal/MidiProbe.cs
+++ Packages/jp.keijiro.minis/Runtime/Internal/MidiProbe.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Threading;
 using Marshal = System.Runtime.InteropServices.Marshal;
 using RtMidiDll = RtMidi.Unmanaged;
 
@@ -10,18 +12,62 @@ namespace Minis
     //
     unsafe sealed class MidiProbe : System.IDisposable
     {
+        Thread _thread;
+        bool _runThread;
         RtMidiDll.Wrapper* _rtmidi;
+        static int _portCount = 0;
+        static List<string> _portNames = new List<string>();
+        int _localPortCount = 0;
+        List<string> _localPortNames = new List<string>();
 
         public MidiProbe()
         {
             _rtmidi = RtMidiDll.InCreateDefault();
 
             if (_rtmidi == null || !_rtmidi->ok)
+            {
                 UnityEngine.Debug.LogWarning("Failed to create an RtMidi device object.");
+                return;
+            }
+
+            _runThread = true;
+            _thread = new Thread(() =>
+            {
+                while(_runThread)
+                {
+                    var portCount = (int)RtMidiDll.GetPortCount(_rtmidi);
+                    var portNames = new List<string>();
+                    for (int n = 0; n < portCount; n++)
+                    {
+                        portNames.Add(Marshal.PtrToStringAnsi(RtMidiDll.GetPortName(_rtmidi, (uint)n)));
+                    }
+
+                    Monitor.Enter(_portNames);
+                    try
+                    {
+                        _portCount = portCount;
+                        _portNames = portNames;
+                    }
+                    finally
+                    {
+                        Monitor.Exit(_portNames);
+                    }
+
+                    Thread.Sleep(1000 / 30);
+                }
+            });
+            _thread.Start();
         }
 
         ~MidiProbe()
         {
+            if (_thread != null)
+            {
+                _runThread = false;
+                _thread.Join();
+                _thread = null;
+            }
+
             if (_rtmidi == null || !_rtmidi->ok) return;
             RtMidiDll.InFree(_rtmidi);
         }
@@ -38,15 +84,28 @@ namespace Minis
 
         public int PortCount {
             get {
-                if (_rtmidi == null || !_rtmidi->ok) return 0;
-                return (int)RtMidiDll.GetPortCount(_rtmidi);
+                if (Monitor.TryEnter(_portNames))
+                {
+                    try
+                    {
+                        _localPortCount = _portCount;
+                        _localPortNames = _portNames;
+                    }
+                    finally
+                    {
+                        Monitor.Exit(_portNames);
+                    }
+                }
+
+                return _localPortCount;
             }
         }
 
         public string GetPortName(int portNumber)
         {
             if (_rtmidi == null || !_rtmidi->ok) return null;
-            return Marshal.PtrToStringAnsi(RtMidiDll.GetPortName(_rtmidi, (uint)portNumber));
+
+            return portNumber >= _localPortNames.Count ? null : _localPortNames[portNumber];
         }
     }
 }
